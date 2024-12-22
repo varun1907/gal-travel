bb7ba1b69d4ca4541d50e30b5b83b7f1de459bef
Prateek Jain <72343114+prateekjain6342@users.noreply.github.com>
2024-12-22 12:36:31 +0530

    Added published filter for recent guides

--- app/guide/[slug]/page.tsx
+++ app/guide/[slug]/page.tsx
@@ -44,6 +44,7 @@ async function fetchLatestGuides(slug: string) {
     "fields[]":
       "id,slug,country,blog_listing_preview_image.*,blog_listing_cta_text,blog_listing_preview_text",
     "sort[]": "-date_created",
+    "filter[status][_eq]": "published"
   };
 
   try {
