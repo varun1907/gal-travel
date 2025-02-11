519dce23a6b409e88ae5347730870df2966938ba
Prateek Jain <72343114+prateekjain6342@users.noreply.github.com>
2025-02-11 21:27:31 +0530

    Updated TTL

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   images: {
     domains: ["admin.galtravelbychandni.com"], // Add your domain here
-    minimumCacheTTL: 30
+    minimumCacheTTL: 60
   },
   eslint: {
     ignoreDuringBuilds: true,
